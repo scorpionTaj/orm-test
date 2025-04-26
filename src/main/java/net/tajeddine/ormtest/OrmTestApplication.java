a1a50d109dac12505182a255729485eccc0a3e51
Tajeddine Bourhim <bourhimtajeddine@gmail.com>
2025-04-26 10:05:21 +0100

    Add initial application setup with Product entity and repository
    - Configure application properties for H2 database and server port
    - Implement Product entity with JPA annotations
    - Create ProductRepository interface extending JpaRepository
    - Add CommandLineRunner to populate database with sample products

--- src/main/java/net/tajeddine/ormtest/OrmTestApplication.java
+++ src/main/java/net/tajeddine/ormtest/OrmTestApplication.java
@@ -1,13 +1,33 @@
 package net.tajeddine.ormtest;
 
+import net.tajeddine.ormtest.entities.Product;
+import net.tajeddine.ormtest.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class OrmTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrmTestApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrmTestApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args ->  {
+            Product p = Product.builder().name("Computer").price(5400).quantity(5).build();
+            productRepository.save(p);
+            productRepository.save(Product.builder().name("Printer").quantity(4).price(640).build());
+            productRepository.save(Product.builder().name("Smart Phone").quantity(34).price(1200).build());
+            List<Product> products = productRepository.findByNameContainsIgnoreCase("M");
+            products.forEach(prod -> {;
+                System.out.println(prod.toString());
+            });
+        };
+    }
 
 }
--- src/main/java/net/tajeddine/ormtest/repository/ProductRepository.java
+++ src/main/java/net/tajeddine/ormtest/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package net.tajeddine.ormtest.repository;
 
-public interface ProductRepository {
+import net.tajeddine.ormtest.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameContainsIgnoreCase(String kw);
 }
